74bafb1abc29fa4b3d373cf0ba53d8f15879b939
RSijelmass <R.Sijelmass@gmail.com>
2017-07-31 15:48:13 +0100

    Creates #split_by_delimiter - by default sets ',' as delimiter - replaces \n to delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
 
 	def add(string)
-		string.split("").inject(0) { |sum, char| sum + char.to_i }
+		split_string = split_by_delimiter(string)
+		split_string.inject(0) { |sum, char| sum + char.to_i }
+	end
+
+	def split_by_delimiter(string, delimiter = ',')
+		string.gsub!('\n', delimiter)
+		return string.split(delimiter)
 	end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,19 +3,30 @@ require 'string_calculator'
 describe StringCalculator do
 	let(:calculator) { StringCalculator.new }
 	
-	it 'adds two numbers together from a string' do
-		expect(calculator.add('1,2')).to eq 3
-	end
-	it 'adds 1 number and returns it from a string' do
-		expect(calculator.add('1')).to eq 1
+	describe 'adds numbers with default delimiter ","' do	
+		it 'adds two numbers together from a string' do
+			expect(calculator.add('1,2')).to eq 3
+		end
+		it 'adds 1 number and returns it from a string' do
+			expect(calculator.add('1')).to eq 1
+		end
+
+		it 'takes 0 numbers, and returns 0' do
+			expect(calculator.add('no,numbers,here!')).to eq 0
+		end
+
+		it 'takes in 5 numbers from a string, and returns the sum' do
+			expect(calculator.add('1,2,3,4,five,6')).to eq 16
+		end
+
+		it 'handles lines ("\n") within a string' do
+			expect(calculator.add('1,2\n3')).to eq 6
+		end
 	end
 
-	it 'takes 0 numbers, and returns 0' do
-		expect(calculator.add('no numbers here!')).to eq 0
-	end
-
-	it 'takes in 5 numbers from a string, and returns the sum' do
-		expect(calculator.add('1,2,3,4,five, 6')).to eq 16
+	describe 'handles delimiters' do
+		it 'takes a different delimiter' do
+		end
 	end
 end
 
