d9caed7a3ac8f794b533ea9a01f64ee8f582ce15
RSijelmass <R.Sijelmass@gmail.com>
2017-07-31 15:34:34 +0100

    Tests for edge cases of #add added

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,8 +2,20 @@ require 'string_calculator'
 
 describe StringCalculator do
 	let(:calculator) { StringCalculator.new }
+	
 	it 'adds two numbers together from a string' do
-			expect(calculator.add('1,2')).to eq 3
+		expect(calculator.add('1,2')).to eq 3
+	end
+	it 'adds 1 number and returns it from a string' do
+		expect(calculator.add('1')).to eq 1
+	end
+
+	it 'takes 0 numbers, and returns 0' do
+		expect(calculator.add('no numbers here!')).to eq 0
+	end
+
+	it 'takes in 5 numbers from a string, and returns the sum' do
+		expect(calculator.add('1,2,3,4,five, 6')).to eq 16
 	end
 end
 
